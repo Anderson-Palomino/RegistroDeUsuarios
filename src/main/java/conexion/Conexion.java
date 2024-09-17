ac86817913bce28f2e65b9efa3bcb212ed3999f9
UNDERDOG2301 <andersonpalominosandoval@gmail.com>
2024-09-17 09:52:29 -0500

    Conexion BD

---
+++ src/main/java/conexion/Conexion.java
@@ -0,0 +1,30 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+
+    public static Connection getConexion() {
+        Connection conexion = null;
+        String baseDatos = "registro_usuarios";
+        String url = "jdbc:mysql://localhost:3306/" + baseDatos;
+        String usuario = "root";
+        String password = "root";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+        } catch (Exception e) {
+            System.out.println("Error al conectar con la base de datos "+e.getMessage());
+        }
+        return conexion;
+    }
+    public static void main(String[] args) {
+        Connection conexion = Conexion.getConexion();
+        if(conexion != null){
+            System.out.println("Conexion establecida "+ conexion);
+        }else{
+            System.out.println("Conexion no establecida");
+        }
+    }
+}
